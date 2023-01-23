f0443214c8654b87f1f2678b927922027079f16a
Raissa <raissagoncalvesdo@gmail.com>
2023-01-23 09:43:15 -0300

    exercício concluído

---
+++ fundamentos/secao3/dia01/ex1-tipos-primitivos.js
@@ -0,0 +1,18 @@
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+typeof patientId;
+typeof isEnrolled;
+typeof patientInfo;
+typeof patientEmail;
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
