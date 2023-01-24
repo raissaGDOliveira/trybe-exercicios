5c1caaf38f0c64835c854d31d004410d06dbb85a
Raissa <raissagoncalvesdo@gmail.com>
2023-01-24 16:25:20 -0300

    atualizando exercicio

--- fundamentos/secao3/dia02/exercicios-pratica.js
+++ fundamentos/secao3/dia02/exercicios-pratica.js
@@ -50,12 +50,4 @@ for (index = 0; index < numbers.length; index += 1) {
     }
 }
 console.log(lowerNumber);
-//Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
-let numeros = [];
-
-for(index = 0; index <= 25; index += 1) {
-    numeros.push(index);
-}
-console.log(numeros);
-//Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
 
