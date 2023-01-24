85f835a489d59e67e45544c780ad1e65c25a0a5b
Raissa <raissagoncalvesdo@gmail.com>
2023-01-23 21:19:00 -0300

    Exercício concluído

---
+++ fundamentos/secao3/dia02/arrays.js
@@ -0,0 +1,13 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// Obtenha o valor “Serviços” do array menu:
+let menuServices = menu[1];
+console.log(menuServices);
+
+// Procure o índice do valor “Portfólio” do array menu:
+let indexOfPortfolio = menu[3];
+console.log(indexOfPortfolio);
+
+// Adicione o valor “Contato” no final do array menu:
+menu.push = 'Contato';
+console.log(menu);
