33a2fe8965ad9a17c4759fb5830ec018fa318ef2
Raissa <raissagoncalvesdo@gmail.com>
2023-01-25 09:30:12 -0300

    exercicio fatorial consluído

---
+++ fundamentos/secao3/dia03/exercicios-pratica.js
@@ -0,0 +1,7 @@
+//crie um algoritmo que imprima na tela o fatorial de 10.
+let factorialNumber = 10;
+
+for (i=9; i>0; i--) {
+    factorialNumber = i * factorialNumber;
+}
+console.log(factorialNumber);
