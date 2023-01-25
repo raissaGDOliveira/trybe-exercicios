cf8e2a3c0c10435cfe2737b6e6e9bde5b7cf1536
Raissa <raissagoncalvesdo@gmail.com>
2023-01-25 10:32:03 -0300

    exercicio palindromo concluido

---
+++ fundamentos/secao3/dia03/exercicio-palindromo.js
@@ -0,0 +1,10 @@
+//Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra.
+//Por exemplo, a palavra “banana” seria invertida para “ananab”.
+//Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+let word = 'tryber';
+let palindromo = '';
+
+for (index = 0; index < word.length; index++) {
+    palindromo += word[word.length - 1 - index];
+}
+console.log(palindromo);
