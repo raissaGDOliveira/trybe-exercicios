69d4d38220e4008dbcf3e337a2b5a94a61fb6d21
Raissa <raissagoncalvesdo@gmail.com>
2023-01-26 20:18:28 -0300

    exercicio function operações concluido

---
+++ fundamentos/secao3/dia-04-objetos-e-funcoes/ex-function.js
@@ -0,0 +1,28 @@
+//Faça cinco programas, um para cada operação aritmética básica.
+//Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados.
+//Faça programas para:
+//Adição (a + b)
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(2, 5));
+//Subtração (a - b)
+function sub(a, b) {
+    return a - b;
+}
+console.log(sub(2, 5));
+//Multiplicação (a * b)
+function multip(a, b) {
+    return a * b;
+}
+console.log(multip(2, 5));
+//Divisão (a / b)
+function div(a, b) {
+    return a / b;
+}
+console.log(div(2, 5));
+//Módulo (a % b)
+function modl(a, b) {
+    return a % b;
+}
+console.log(modl(5, 5));
